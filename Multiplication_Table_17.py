6ba6533d273fe070bc2902596466c861c721a781
Louis-Sunil <162080487+Louis-Sunil@users.noreply.github.com>
2024-03-12 12:47:21 +0530

    updated \t in line number 8 to print = in uniform manner

--- Multiplication_Table_17.py
+++ Multiplication_Table_17.py
@@ -5,5 +5,5 @@ count = 1
 
 while count < 11:
     multiply_output = multiply_of * count
-    print(multiply_of,"*",count,"=",multiply_output)
+    print(multiply_of,"*",count," \t=",multiply_output)
     count += 1
