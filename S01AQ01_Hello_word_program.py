e94a497936cca8305c093dfd1118831fd371421e
Louis-Sunil <162080487+Louis-Sunil@users.noreply.github.com>
2024-03-14 22:41:57 +0530

    Hello world program using functions

---
+++ S01AQ01_Hello_word_program.py
@@ -0,0 +1,34 @@
+""" 
+S01AQ01 : Hello World program to the user
+"""
+
+def get_username():
+    """ 
+    Prompt the user to enter his name
+    Input : None
+    Output : Return the user input as a string
+    """
+    # Your solution code should go in here
+    usr_name = input("Hey whats your Name ?")
+    return usr_name
+
+def say_hello(user):
+    '''
+    Greet the user by saying Hello followed by his name
+    Input : user name
+    Output : Greeting statement on the screen. No return value
+    Example : If input string is Sam
+        Then the expected output is
+        Hello Sam !!!
+    '''
+    print("Hello", user,"!!!")
+
+def main():
+    '''
+    Main function of the program
+    '''
+    name = get_username()
+    say_hello(name)
+    
+# Main starts from below
+main()
